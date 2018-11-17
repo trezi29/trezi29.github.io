7e37a897f61d85b05acf768ecfeebf1aea68d4e5
trezi29 <trezi29@gmail.com>
2018-11-17 11:03:39 +0100

    fiers commit on dev branch

---
+++ src/components/slideshow.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Slideshow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: 0, // set the current image number in array
+      imgNumber: this.props.images.length // reads the number of images in props array
+    };
+  }
+
+  nextImg = () => {
+    this.setState({ img: this.state.img + 1});
+  }
+
+  prevImg = () => {
+    this.setState({ img: this.state.img - 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.images[this.state.img].title}</h1>
+        <h2>{this.props.images[this.state.img].date}</h2>
+        <img width="100%" src={this.props.images[this.state.img].image}></img>
+        {this.state.img < this.state.imgNumber-1 ? <button onClick={this.nextImg}>Next</button> : ''}
+        {this.state.img > 0 ? <button onClick={this.prevImg}>Prev</button> : ''}
+      </div>
+    );
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Slideshow from "../components/slideshow"
+
+const imagesList = [
+  {
+    image: '../images/cartasu.ga_01.jpg',
+    title: 'Orobie Ronde 01',
+    date: 'Agosto 2018'
+ },
+  {
+    image: '../images/cartasu.ga_02.jpg',
+    title: 'Orobie Ronde 02',
+    date: 'Agosto 2018'
+  }
+];
+
+const IndexPage = () => (
+  <div>
+    <Slideshow
+      images={imagesList}
+      // title="Orobie Ronde"
+      // date="Agosto 2018"
+    />
+  </div>
+)
+
+export default IndexPage
