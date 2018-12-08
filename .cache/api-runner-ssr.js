13232b66d4e65c0b404e87a1fa3874359a1af8a5
trezi29 <trezi29@gmail.com>
2018-12-08 14:04:54 +0100

    adds favicon

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/matteotressi/Documents/GitHub/trezi29.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui"},
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/icon/cartasuga_icon.png"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
