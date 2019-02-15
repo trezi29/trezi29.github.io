af3a4b4a1d12b575ce13cc703ccf82204ab78aeb
Matteo Tressi <trezi29@gmail.com>
2019-02-15 18:10:35 +0100

    fix minor manifest and css bug on mobile

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/user/Documents/GitHub/trezi29.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/icon/cartasuga_icon.png"},
+      options: {"plugins":[],"name":"cartasu.ga","short_name":"cartasu.ga","start_url":"/","background_color":"#fff","theme_color":"#fff","display":"fullscreen","icon":"src/icon/cartasuga_icon.png"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
--- gatsby-config.js
+++ gatsby-config.js
@@ -17,12 +17,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'cartasu.ga',
+        short_name: 'cartasu.ga',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        background_color: '#fff',
+        theme_color: '#fff',
+        display: 'fullscreen',
         icon: 'src/icon/cartasuga_icon.png', // This path is relative to the root of the site. TODO
       },
     },
